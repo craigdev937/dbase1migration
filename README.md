4a967a5a039199f8ad48ca88b366b5a08b97bbb3
Craig <indigomx9@gmail.com>
2022-08-18 22:01:08 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# TypeORM
+
+Initial setup of the project.  Created with Express, TypeScript, PostgreSQL, and TypeORM.
+
+
+
---
+++ src/routes/userRt.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { indexHome } from "../controllers/userCon";
+
+export const userRt: express.Router = express.Router();
+    userRt.get("/", indexHome);
+
+
